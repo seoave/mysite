df0a3638cbfb6b6fd1e79a000dd3ad2f9c01b778
seoave <seo.ave@gmail.com>
2020-12-22 11:31:18 +0200

    задание 3

--- index.php
+++ index.php
@@ -2,12 +2,31 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    <h1><?php echo 'Hello, NIX Education' ?></h1>
+    <header class="header">
+        <a class="site-logo" href="/">NIX Education</a>
+        <nav class="nav">
+            <ul class="menu">
+                <li class="menu-item"><a href="#">Урок 1</a></li>
+                <li class="menu-item"><a href="#">Урок 2</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main">
+        <h1><?php echo 'Hello, NIX Education' ?></h1>
+    </main>
+    <footer class="footer">
+        <span class="copyright">(c) 2020</span>
+    </footer>
 </body>
+
 </html>
